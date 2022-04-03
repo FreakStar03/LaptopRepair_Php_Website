26827d261e1c3ad616d47880754c12b6318a9d40
ChiragPadyal <freakstar03@gmail.com>
2022-04-03 22:16:23 +0530

    changed docker compose

--- resources/config.php
+++ resources/config.php
@@ -1,8 +1,8 @@
 <?php
 
-$servername = "localhost";
-$username = "chiragsp";
-$password = "admin";
+$servername = "127.0.0.1";
+$username = "root";
+$password = "root";
 $dbname = "laptop_repair";
 $port = "3307";
   
